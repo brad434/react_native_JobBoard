e57d3cce61d9e7a817df0d8c7acff2659b690138
Bradley Chery <Bradley434@hotmail.com>
2023-07-31 21:46:56 -0400

    initial files

--- app/_layout.js
+++ app/_layout.js
@@ -1,7 +1,29 @@
 import { Stack } from "expo-router";
+import { useCallback } from "react";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+// import "../assets/fonts/DMSans-Bold.ttf";
+
+SplashScreen.preventAutoHideAsync();
 
 const Layout = () => {
-  return <Stack />;
+  // created a custom font to be used for later. This font is located in the assets folder
+  const [fontsLoaded] = useFonts({
+    DMBold: require("../assets/fonts/DMSans-Bold.ttf"),
+    DMMedium: require("../assets/fonts/DMSans-Medium.ttf"),
+    DMRegular: require("../assets/fonts/DMSans-Regular.ttf"),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    // it will only show the homepage if the fonts has been loaded.
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) return null;
+
+  return <Stack onLayout={onLayoutRootView} />;
 };
 
 export default Layout;
